32c6e161140eb764d186dea7e40b5fd73e3b15ad
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-07-05 19:30:24 +0200

    logic for next question in the quest

--- src/main/java/com/quest/NextQuestionServlet.java
+++ src/main/java/com/quest/NextQuestionServlet.java
@@ -1,4 +1,46 @@
 package com.quest;
 
-public class NextQuestionServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/next")
+public class NextQuestionServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String answer = req.getParameter("answer");
+        String currentNodeQuestion = req.getParameter("currentNode");
+
+        TreeNode currentNode = findNodeByQuestion((TreeNode) getServletContext().getAttribute("root"), currentNodeQuestion);
+        TreeNode nextNode = "Yes".equals(answer) ? currentNode.getYesBranch() : currentNode.getNoBranch();
+
+        req.setAttribute("username", username);
+        if (nextNode != null) {
+            req.setAttribute("question", nextNode.getQuestion());
+            req.setAttribute("yesBranch", nextNode.getYesBranch());
+            req.setAttribute("noBranch", nextNode.getNoBranch());
+            req.getRequestDispatcher("/question.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/end.jsp").forward(req, resp);
+        }
+    }
+
+    private TreeNode findNodeByQuestion(TreeNode root, String question) {
+        if (root == null) {
+            return null;
+        }
+        if (root.getQuestion().equals(question)) {
+            return root;
+        }
+        TreeNode foundNode = findNodeByQuestion(root.getYesBranch(), question);
+        if (foundNode == null) {
+            foundNode = findNodeByQuestion(root.getNoBranch(), question);
+        }
+        return foundNode;
+    }
 }
+
