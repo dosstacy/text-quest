f9bd027b438577f6f67b66d08859947f2fdc64c0
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-07-05 20:26:16 +0200

    RestartServlet to restart quest

---
+++ src/main/java/com/quest/RestartServlet.java
@@ -0,0 +1,27 @@
+package com.quest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/restart")
+public class RestartServlet extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger(RestartServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        LOGGER.info("Invalidating session and redirecting to /start");
+        try {
+            req.getSession().invalidate();
+            resp.sendRedirect("/start");
+        }catch (IOException e){
+            LOGGER.error("IOException occurred while redirecting to /start", e);
+            throw e;
+        }
+    }
+}
