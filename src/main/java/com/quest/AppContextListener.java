6e0f670e11439ec294448943ddf11505f6a8c54c
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-07-07 15:13:26 +0200

    added log to AppContextListener

--- src/main/java/com/quest/AppContextListener.java
+++ src/main/java/com/quest/AppContextListener.java
@@ -1,5 +1,8 @@
 package com.quest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -8,6 +11,7 @@ import java.nio.file.Paths;
 
 @WebListener
 public class AppContextListener implements ServletContextListener {
+    private static final Logger LOGGER = LogManager.getLogger(StartServlet.class);
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -15,7 +19,7 @@ public class AppContextListener implements ServletContextListener {
             TreeNode root = new TreeBuilder().buildTreeFromJson(Paths.get("src", "main", "resources", "questions.json").toString());
             sce.getServletContext().setAttribute("root", root);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("Failed to initialize context due to IOException: {}", e.getMessage(), e);
         }
     }
 }
